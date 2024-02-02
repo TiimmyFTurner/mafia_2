60657f4243a01d6fa770e12ffd7937a0f5f44582
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-02-02 15:58:23 +0330

    signig app with release sign

--- lib/presentation/screens/set_players_screen.dart
+++ lib/presentation/screens/set_players_screen.dart
@@ -25,107 +25,110 @@ class _SetPlayersScreenState extends ConsumerState<SetPlayersScreen> {
   @override
   Widget build(BuildContext context) {
     List players = ref.watch(playerNamesProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("انتخاب بازیکن ها (${players.length})"),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.home),
-          onPressed: () => context.pushReplacementNamed('homeScreen'),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.help_outline),
-            onPressed: () {
-              showDialog<void>(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text("راهنما"),
-                    content: const Text(
-                        'در این قسمت شما میتوانید به هر تعدادی میخواهید بازیکن اضافه کنید'),
-                    actions: <Widget>[
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          textStyle: Theme.of(context).textTheme.labelLarge,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("انتخاب بازیکن ها (${players.length})"),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () => context.pushReplacementNamed('homeScreen'),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.help_outline),
+              onPressed: () {
+                showDialog<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("راهنما"),
+                      content: const Text(
+                          'در این قسمت شما میتوانید به هر تعدادی میخواهید بازیکن اضافه کنید'),
+                      actions: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: Theme.of(context).textTheme.labelLarge,
+                          ),
+                          child: const Text("بازگشت"),
+                          onPressed: () {
+                            context.pop();
+                          },
                         ),
-                        child: const Text("بازگشت"),
-                        onPressed: () {
-                          context.pop();
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 8),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: TextField(
-              controller: _controller,
-              onChanged: (String value) => _name = value,
-              onSubmitted: _addPlayer,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                labelText: 'نام بازیکن',
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: _addPlayer,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: ListView.builder(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                shrinkWrap: true,
-                itemCount: players.length + 1,
-                itemBuilder: (BuildContext context, int index) {
-                  return index < players.length
-                      ? listItemPlayer(players[index])
-                      : const SizedBox(height: 8);
-                }),
-          ),
-          SizedBox(
-            height: 66,
-            width: MediaQuery.of(context).size.width,
-            child: Hero(
-              tag: "play",
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: FilledButton(
-                  child: const Text(
-                    "انتخاب نقش",
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  onPressed: () {
-                    if (players.length > 2) {
-                      ref.read(customRoleProvider.notifier).recoverRoles();
-                      context.pushNamed('setRoles');
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content:
-                              Text("حداقل تعداد بازیکنان باید سه نفر باشد"),
-                        ),
-                      );
-                    }
+                      ],
+                    );
                   },
+                );
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                controller: _controller,
+                onChanged: (String value) => _name = value,
+                onSubmitted: _addPlayer,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'نام بازیکن',
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: _addPlayer,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 8),
+            Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  shrinkWrap: true,
+                  itemCount: players.length + 1,
+                  itemBuilder: (BuildContext context, int index) {
+                    return index < players.length
+                        ? listItemPlayer(players[index])
+                        : const SizedBox(height: 8);
+                  }),
+            ),
+            SizedBox(
+              height: 66,
+              width: MediaQuery.of(context).size.width,
+              child: Hero(
+                tag: "play",
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: FilledButton(
+                    child: const Text(
+                      "انتخاب نقش",
+                      style: TextStyle(fontSize: 22),
+                    ),
+                    onPressed: () {
+                      if (players.length > 2) {
+                        ref.read(customRoleProvider.notifier).recoverRoles();
+                        context.pushNamed('setRoles');
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content:
+                                Text("حداقل تعداد بازیکنان باید سه نفر باشد"),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
