b820dcf757fee81b59b7fdb98cacc16eeeed4114
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-02-12 16:00:27 +0330

    create themeMode provider

---
+++ lib/applications/state_management/settings_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:mafia_2/applications/state_management/shared_preferences_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'settings_provider.g.dart';
+
+@riverpod
+class ThemeModeSetting extends _$ThemeModeSetting {
+  dynamic _prefs;
+
+  @override
+  ThemeMode build() {
+    _prefs = ref.watch(sharedPreferencesProvider);
+    final themeMode = _prefs.getInt('themeMode');
+    return switch (themeMode) {
+      1 => ThemeMode.light,
+      2 => ThemeMode.dark,
+      _ => ThemeMode.system
+    };
+  }
+
+  void changeTheme(ThemeMode themeMode) {
+    state = themeMode;
+    switch (themeMode) {
+      case ThemeMode.light:
+        _prefs.setInt('themeMode', 1);
+      case ThemeMode.dark:
+        _prefs.setInt('themeMode', 2);
+      default:
+        _prefs.remove('themeMode');
+    }
+  }
+}
